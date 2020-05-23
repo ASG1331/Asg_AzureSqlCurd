48588269b1c784186f380da89d4e0e43a2c98dc0
Aarti Shinde <aartishinde31@gmail.com>
2020-05-23 11:49:35 +0530

    Add project files.

---
+++ AzureSqlWebApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AzureSqlWebApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AzureSqlWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureSqlWebApplication.MvcApplication" Language="C#" %>
